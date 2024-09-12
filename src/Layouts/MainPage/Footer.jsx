d3a268257aff8626a3ae780d0720c3c32268bfdd
amirjavani <amirjavani@outlook.com>
2024-09-12 15:00:13 +0330

    adding simple footer

---
+++ src/Layouts/MainPage/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import './Footer.css';
+const Footer = (props) => {
+  return (
+    <div className="h-[100px] bg-black text-center">
+         Footer
+    </div>
+  )
+};
+
+export default Footer;
